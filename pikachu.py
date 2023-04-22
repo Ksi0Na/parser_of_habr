e832d64942716bae301622034ae5989fad50c15c
Ksi0Na <OlgaMolchun20010615@yandex.ru>
2023-04-22 03:08:31 +0300

    v2.1

--- pikachu.py
+++ pikachu.py
@@ -124,4 +124,3 @@ class RabbitMQ:
         await self.channel.bind_queue(queue_name=self.queue_name,
                                       exchange_name=self.exchange_name,
                                       routing_key='')
-
